67ff0da79994860e54a064a357f8d04562aa1da5
Yousef El-Bsauony <youssefmohamedelbasouny@gmail.com>
2025-05-07 17:43:38 +0300

    Add project files.

---
+++ items.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Seession3_
+{
+    public class items
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Priority { get; set; }
+
+
+        public void display()
+        {
+            Console.WriteLine($"titlel:{Title}");
+            Console.WriteLine($"descrapitio:{Description}");
+            Console.WriteLine($"date:{Date}");
+            Console.WriteLine($"priority:{Priority}");
+            Console.WriteLine("**************************");
+        }
+    }
+}

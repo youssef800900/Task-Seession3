4a3692a3699f132a524f86981a63f1831b5c8c2f
Yousef El-Bsauony <youssefmohamedelbasouny@gmail.com>
2025-05-09 14:34:56 +0300

    Revert "Update .gitignore to keep only Program.cs and items.cs"
    This reverts commit 838d5a970558d4dab5a2737c84f9abb6bfbd1783.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Task_Seession3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<items> tasks = new List<items>();
+
+            tasks.Add(new items { Title = "Task 1", Description = "First task", Date = DateTime.Now.AddDays(2), Priority = "High" });
+            tasks.Add(new items { Title = "Task 2", Description = "Second task", Date = DateTime.Now.AddDays(3), Priority = "Low" });
+            tasks.Add(new items { Title = "Task 3", Description = "Third task", Date = DateTime.Now.AddDays(5), Priority = "Medium" });
+
+            foreach (var item in tasks)
+            {
+                item.display();
+            }
+            Console.WriteLine("Press to exit..");
+            Console.ReadKey();
+        }
+    }
+}
